d65432ddf2755ee23de32325595b7d88b8572d9f
NoTiger <stevenyhtyeh@gmail.com>
2020-01-17 01:03:39 +0800

    gigit cmd hello world

--- resume/src/App.vue
+++ resume/src/App.vue
@@ -1,44 +1,54 @@
 <template>
   <div id="app">
-      <div class="home-card">
+    <transition>
+      <div v-bind:class="{ homeCardFullHeight: isVisited, homeCard: !isVisited }">
         <div class="home-nav">
           <router-link to="/">Home</router-link> |
           <router-link to="/about">About</router-link>
         </div>
-        <br/>
-        <br/>
-        <button v-on:click="scrollActivated = !scrollActivated" type="button">Test</button>
+        <br />
+        <br />
+        <button class="ml-3" v-on:click="scrollActivated = !scrollActivated" type="button">
+          Test scrollable
+        </button>
+        <button class="ml-3" v-on:click="isVisited = !isVisited" type="button">
+          Test Height
+        </button>
       </div>
-      <h1>Hello World</h1>
+    </transition>
 
+    <h1>Hello World</h1>
 
-     <div v-if="isVisited">
-     <router-view />
-     </div>
+    <div> <!-- v-if="isVisited" -->
+      <router-view />
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: "App",
-  data: function(){
-      return {
-          isVisited: false,
-          scrollActivated: false
-      }
+  data: function() {
+    return {
+      isVisited: false,
+      scrollActivated: false
+    };
   },
   watch: {
-    scrollActivated: function () {
-      alert('value changed');
-      document.body.className = this.scrollActivated ? 'scrollable' : 'not-scrollable';
+    scrollActivated: function() {
+      alert("value changed");
+      document.body.className = this.scrollActivated
+        ? "scrollable"
+        : "not-scrollable";
     }
   },
   beforeCreate: function() {
-    document.body.className = 'not-scrollable';
+    document.body.className = "not-scrollable";
   },
   methods: {
     testMethod: function() {
-        this.scrollActivated = !this.scrollActivated;
+      this.scrollActivated = !this.scrollActivated;
     }
   }
 };
@@ -46,18 +56,24 @@ export default {
 
 <style>
 body.not-scrollable {
-    overflow:hidden;
-    margin: 0px;
+  overflow: hidden;
+  margin: 0px;
 }
 
 body.scrollable {
-    margin: 0px;
+  margin: 0px;
 }
 
-.home-card {
-    background-color: blue;
-    height: 100vh;
-    width: 100vw;
+.homeCardFullHeight {
+  background-color: blue;
+  height: 100vh;
+  width: 100vw;
+}
+
+.homeCard {
+  background-color: blue;
+  height: 65vh;
+  width: 100vw;
 }
 
 .home-nav {
