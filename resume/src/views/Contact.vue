f2d2eb273d8bb97f8dfc41bdb2e76f8feb60d306
Hsuan Ting, Yeh <stevenyhtyeh@gmail.com>
2020-01-29 22:38:03 +0800

    20200129
    20200129

--- resume/src/views/Contact.vue
+++ resume/src/views/Contact.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container-fluid p-0 m-0 mt-4">
-    <div class="row justify-content-center text-center mb-2">
-      <div class="col-10">
+    <div class="row justify-content-center text-center">
+      <div class="col-10 mb-2">
         <div class="contact-card">
           <h5><i class="fas fa-phone-alt"></i> Telephone</h5>
           <p class="ml-3"><strong>+886 909145502</strong></p>
         </div>
       </div>
     </div>
-    <div class="row justify-content-center text-center mt-4 mb-2">
-      <div class="col-10">
+    <div class="row justify-content-center text-center">
+      <div class="col-10 mt-4 mb-2">
         <div class="contact-card">
           <h5><i class="far fa-envelope"></i> Email</h5>
           <p class="ml-3"><strong>stevenyhtyeh@gmail.com</strong></p>
         </div>
       </div>
     </div>
-    <div class="row justify-content-center text-center mt-4 mb-2">
-      <div class="col-10">
+    <div class="row justify-content-center text-center">
+      <div class="col-10 mt-4 mb-2">
         <div class="contact-card">
           <h5><i class="fab fa-github"></i> Github</h5>
           <a href="https://github.com/NoTiger">
@@ -26,8 +26,8 @@
         </div>
       </div>
     </div>
-    <div class="row justify-content-center text-center mt-4 mb-2">
-      <div class="col-10">
+    <div class="row justify-content-center text-center">
+      <div class="col-10 mt-4 mb-2">
         <div class="contact-card">
           <h5><i class="fab fa-linkedin"></i> Linkedin</h5>
           <a href="https://linkedin.com/in/hsuan-ting-yeh-4a890b161">
